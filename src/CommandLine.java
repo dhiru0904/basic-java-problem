05c56f1bf8867d625a0bfc839606da7eefaa5a06
dhiru0904 <dhirajdevre4@gmail.com>
2022-11-22 19:13:45 +0530

    Added:display name using the command line.

---
+++ src/CommandLine.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CommandLine {
+    public static void main(String[] args) {
+        String name;
+        System.out.println("Enter a String :");
+        Scanner d =new Scanner(System.in);
+        name=d.next();
+        System.out.println("Entered String Is:"+name);
+
+    }
+}
