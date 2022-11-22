491af15939c298f7f424cb4ac817ba4af3d9d68b
dhiru0904 <dhirajdevre4@gmail.com>
2022-11-22 18:34:34 +0530

    Added: Equal a Two String

---
+++ src/EqualString.java
@@ -0,0 +1,8 @@
+public class EqualString {
+    public static void main(String[] args) {
+        String s1="Dhiraj";
+        String s2="Dhiraj";
+        System.out.println(s1.equals(s2));
+    }
+}
+
